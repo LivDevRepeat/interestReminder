44c7a81f72ddd0ced322e7b6029257ef6b04efc2
LivReTaRDIS <lv.zuhause@gmail.com>
2023-07-28 18:45:54 +0200

    init

---
+++ INTREST/INTRESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  INTRESTApp.swift
+//  INTREST
+//
+//  Created by Lina Vorpahl on 28.07.23.
+//
+
+import SwiftUI
+
+@main
+struct INTRESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
